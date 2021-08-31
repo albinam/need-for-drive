7c011e8fb41917113425606b66b2c28294c1d2be
albinosikm <albinosikm@gmail.com>
2021-08-31 16:57:37 +0400

    V-02 отредактированы адаптивы

--- src/components/OrderInfo/OrderInfo.js
+++ src/components/OrderInfo/OrderInfo.js
@@ -5,18 +5,18 @@ import '../../styles/buttons.scss';
 function OrderInfo() {
     return (
         <div className="order_info_container">
-        <div className="order_info">
-            <div className="order_info_title">Ваш заказ:</div>
-            <ul className="order_info_items">
-                <li className="order_info_items_item">
-                    <div className="order_info_items_item_element">Пункт выдачи</div>
-                    <div className="order_info_items_item_dots"/>
-                    <div className="order_info_items_item_value">Ульяновск, Нариманова 42</div>
-                </li>
-            </ul>
-            <div className="order_info_price">Цена: от 8 000 до 12 000 &#8381;</div>
-            <button className="content_button">Выбрать модель</button>
-        </div>
+            <div className="order_info">
+                <div className="order_info_title">Ваш заказ:</div>
+                <ul className="order_info_items">
+                    <li className="order_info_items_item">
+                        <div className="order_info_items_item_element">Пункт выдачи</div>
+                        <div className="order_info_items_item_dots"/>
+                        <div className="order_info_items_item_value">Ульяновск, Нариманова 42</div>
+                    </li>
+                </ul>
+                <div className="order_info_price">Цена: от 8 000 до 12 000 &#8381;</div>
+                <button className="content_button">Выбрать модель</button>
+            </div>
         </div>
     )
 }
