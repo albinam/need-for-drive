d11e60cfd321875104448c78367357001cf6b369
albinosikm <albinosikm@gmail.com>
2021-08-31 18:50:07 +0400

    V-02 фикс цвета активной вкладки

--- src/components/OrderSteps/OrderSteps.js
+++ src/components/OrderSteps/OrderSteps.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './OrderSteps.scss';
+import classNames from "classnames";
 
 export const steps = [
     "Местоположение",
@@ -10,10 +11,13 @@ export const steps = [
 
 function OrderSteps() {
 
+    const [activeLink] = useState(steps[0]);
+
     return (
         <nav className="steps">
             {steps.map((link) => (
-                <a key={link} href="#!">{link}</a>
+                <a  className={classNames("steps_link", (link === activeLink) ? "active" : null)}
+                    key={link} href="#!">{link}</a>
             ))}
         </nav>
     )
